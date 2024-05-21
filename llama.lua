b110824df5014d0dd02c23340af850d58ebb1fd2
CapsAdmin <eliashogstvedt@gmail.com>
2024-05-21 14:04:54 +0700

    more work

--- llama.lua
+++ llama.lua
@@ -41,11 +41,51 @@ local function ggml_type_from_enum(i)
 	end
 end
 
-local function byteSizeFor(self, numberOfElements)
-	local t = numberOfElements * self.typeSize
-	assert(t % self.blockSize == 0, "Total size must be a multiple of the block size")
-	return t / self.blockSize
+local function get_tensor_size(type, dimensions_map)
+	local ggml_type = ggml_type_from_enum(type)
+
+	local t = 1
+	
+	for i, v in ipairs(dimensions_map) do
+		t = t * v
+	end
+
+	return t
 end
+
+local function get_tensor_byte_size(type, dimensions_map)
+	local t = get_tensor_size(type, dimensions_map)
+	local ggml_type = ggml_type_from_enum(type)
+
+	t = t * ggml_type.typeSize
+	assert(t % ggml_type.blockSize == 0, "Total size must be a multiple of the block size")
+	t = t / ggml_type.blockSize
+
+	return t
+end
+
+local function rope_freq(context_length, head_size, theta)
+	assert(head_size % 2 == 0)
+	local cr = {}
+	local ci = {}
+	local n = 1
+
+	for pos = 0, context_length - 1 do
+		for i = 0, head_size - 1, 2 do
+			local freq = 1.0 / (theta ^ (i / head_size))
+			local val = pos * freq
+			cr[n] = math.cos(val)
+			ci[n] = math.sin(val)
+			n = n + 1
+		end
+	end
+
+	n = n - 1
+	assert(context_length * (head_size / 2) == n)
+	return cr, ci
+end
+
+
 
 local cache = {}
 local cached_info = function(cdecl)
@@ -179,42 +219,46 @@ local function load_gguf(path)
 
 		local ggml_type = reader.UINT32(file)
 		local offset = reader.UINT64(file)
+
 		tensors[i] = {
 			name = name,
+			byte_size = get_tensor_byte_size(ggml_type, dimensions_map),
+			size = get_tensor_size(ggml_type, dimensions_map),
 			ggml_type = ggml_type,
 			offset = offset,
-			dimensions_map = dimensions_map,
 		}
 	end
 
 	local alignment = metadata["general.alignment"] or 32
 	local padding = alignment - (file:seek() % alignment)
 	file:seek("set", file:seek() + padding)
+
 	local tensor_data_offset = file:seek()
 	local tensor_data = file:read()
 
-	for _, tensor in ipairs(tensors) do
-		local dimensions = tensor.dimensions_map
-		local number_of_elements = 1
-
-		for i, v in ipairs(dimensions) do
-			assert(v > 0)
-			number_of_elements = number_of_elements * v
-		end
-
-		local byte_size = byteSizeFor(ggml_type_from_enum(tensor.ggml_type), number_of_elements)
+	for i, tensor in ipairs(tensors) do
 		file:seek("set", tonumber(tensor.offset))
-		local tensor_blob = file:read(tonumber(byte_size))
-		print(#tensor_blob)
+		local tensor_blob = file:read(tonumber(tensor.byte_size))
 
-		break
+		tensor.blob = tensor_blob
 	end
 
-	print(file:seek(), "!?")
-	print(file:seek("end"), "!?")
-	assert(metadata["tokenizer.ggml.model"] == "gpt2")
-	local tokens = metadata["tokenizer.ggml.tokens"]
-	local merge_lines = metadata["tokenizer.ggml.merges"]
+	local tensor_map = {}
+	for i,v in ipairs(tensors) do
+		tensor_map[v.name] = v
+		v.index = i
+	end
+
+	return {
+		metadata = metadata,
+		tensors = tensor_map,
+	}
+end
+
+local function Tokenizer(gguf)
+	assert(gguf.metadata["tokenizer.ggml.model"] == "gpt2")
+	local tokens = gguf.metadata["tokenizer.ggml.tokens"]
+	local merge_lines = gguf.metadata["tokenizer.ggml.merges"]
 	local tokens_map = {}
 
 	for k, v in pairs(tokens) do
@@ -247,59 +291,163 @@ local function load_gguf(path)
 		tk_merges[i] = merge_index
 	end
 
-	local context_length = 10000
-	local model_context_length = metadata["llama.context_length"]
-	local dim = metadata["llama.embedding_length"]
-	local hiddenDim = metadata["llama.feed_forward_length"]
-	local numberOfLayers = metadata["llama.block_count"]
-	local numberOfHeads = metadata["llama.attention.head_count"]
-	local numberOfKeyValueHeads = metadata["llama.attention.head_count_kv"] and
-		metadata["llama.attention.head_count_kv"] or
-		metadata["llama.attention.head_count"]
-	local vocabularySize = #tokens
-	local contextLength = context_length
-	local sharedWeights = false
-	local rmsNormEps = metadata["llama.attention.layer_norm_rms_epsilon"] or 1e-5
-	local ropeTheta = metadata["llama.rope.freq_base"] or 10000
-	local headSize = dim / numberOfHeads
-
-	local function rope_freq(context_length, head_size, theta)
-		assert(head_size % 2 == 0)
-		local cr = {}
-		local ci = {}
-		local n = 1
-
-		for pos = 0, context_length - 1 do
-			for i = 0, head_size - 1, 2 do
-				local freq = 1.0 / (theta ^ (i / head_size))
-				local val = pos * freq
-				cr[n] = math.cos(val)
-				ci[n] = math.sin(val)
-				n = n + 1
-			end
-		end
-
-		n = n - 1
-		assert(context_length * (head_size / 2) == n)
-		return cr, ci
-	end
-
-	print(contextLength, headSize, ropeTheta)
-	local ropeFreqsReal, ropeFreqsImag = rope_freq(contextLength, headSize, ropeTheta)
-
-	local function loadQuantized(entry)
-		local info, name = ggml_type_from_enum(entry.ggml_type)
-
-		if name == "Q4_0" then
-
-		else
-			error("unsupported quant format " .. name)
-		end
-	end
-
-	loadQuantized(tensors["token_embd.weight"])
+	return {
+		tokens = tokens,
+	}
 end
 
-load_gguf(
+
+local gguf = load_gguf(
 	"/home/caps/.ollama/models/blobs/sha256:00e1317cbf74d901080d7100f57580ba8dd8de57203072dc6f668324ba545f29"
 )
+
+local tokenizer = Tokenizer(gguf)
+
+
+
+local context_length = 10000
+local model_context_length = gguf.metadata["llama.context_length"]
+local dim = gguf.metadata["llama.embedding_length"]
+local hiddenDim = gguf.metadata["llama.feed_forward_length"]
+local numberOfLayers = gguf.metadata["llama.block_count"]
+local numberOfHeads = gguf.metadata["llama.attention.head_count"]
+local numberOfKeyValueHeads = gguf.metadata["llama.attention.head_count_kv"] and
+gguf.metadata["llama.attention.head_count_kv"] or
+gguf.metadata["llama.attention.head_count"]
+local vocabularySize = #tokenizer.tokens
+local contextLength = context_length
+local sharedWeights = false
+local rmsNormEps = gguf.metadata["llama.attention.layer_norm_rms_epsilon"] or 1e-5
+local ropeTheta = gguf.metadata["llama.rope.freq_base"] or 10000
+local headSize = dim / numberOfHeads
+
+local ropeFreqsReal, ropeFreqsImag = rope_freq(contextLength, headSize, ropeTheta)
+
+local function Q4_0FloatTensor(size, blob)
+	local self = {}
+	self.size = size
+	self.type = GGMLType.Q4_0
+
+	function self:SetFloat(index, value)
+		error"NYI"
+	end	
+
+	function self:GetFloatVector(index, value)
+		error"NYI"
+	end
+
+	function self:GetFloat(index)
+		local block_index = index / self.type.blockSize
+		local block_offset = block_index * self.type.typeSize
+		local scale = blob[index] -- read uint16_t
+		local quant 
+		local modIndex = index % self.type.blockSize
+		if modIndex < self.type.blockSize / 2 then
+			quant = bit.band(blob[block_offset + FLOAT16 + modIndex], 0x0F)
+		else
+			quant = bit.band(bit.rshift(blob[block_offset + FLOAT16 + modIndex - self.type.blockSize / 2], 4), 0x0F)
+		end
+		quant = quant - 8
+
+		return quant * scale
+	end
+
+	function self:Dot(thisOffset, that, thatOffset, size)
+		local result = 0;
+		for j = 0, size - 1 do
+			result = result + self:GetFloat(thisOffset + j) * that:GetFloat(thatOffset + j);
+		end
+		return result
+	end
+
+	function self:MatMul(that, out, dim0, dim1)
+		for i = 0, dim0 - 1 do
+			out:SetFloat(i, self:Dot(i * dim1, that, 0, dim1))
+		end		
+	end
+	
+	return self
+end
+
+local function FloatTensor(size, blob)
+	local self = {}
+	self.size = size
+	self.type = GGMLType.Q4_0
+
+	function self:SetFloat(index, value)
+		error"NYI"
+	end	
+
+	function self:GetFloatVector(index, value)
+		error"NYI"
+	end
+
+	function self:GetFloat(index)
+		local block_index = index / self.type.blockSize
+		local block_offset = block_index * self.type.typeSize
+		local scale = blob[index] -- read uint16_t
+		local quant 
+		local modIndex = index % self.type.blockSize
+		if modIndex < self.type.blockSize / 2 then
+			quant = bit.band(blob[block_offset + FLOAT16 + modIndex], 0x0F)
+		else
+			quant = bit.band(bit.rshift(blob[block_offset + FLOAT16 + modIndex - self.type.blockSize / 2], 4), 0x0F)
+		end
+		quant = quant - 8
+
+		return quant * scale
+	end
+
+	function self.ScalarDot(self, thisOffset, that, thatOffset, size)
+		local result = 0;
+		for j = 0, size - 1 do
+			result = result + self:GetFloat(thisOffset + j) * that:GetFloat(thatOffset + j);
+		end
+		return result
+	end
+
+	function self:Dot(thisOffset, that, thatOffset, size)
+		return self.ScalarDot(self, thisOffset, that, thatOffset, size)
+	end
+
+	function self:MatMul(that, out, dim0, dim1)
+		for i = 0, dim0 - 1 do
+			out:SetFloat(i, self:Dot(i * dim1, that, 0, dim1))
+		end		
+	end
+
+	function self:Reduce(thisOffset, size, seed, reduce_callback)
+		local result = seed
+		for i = 0, size - 1 do
+			result = reduce_callback(result, self:GetFloat(thisOffset + i))
+		end
+		return result
+	end
+
+	function self:Sum(thisOffset, size)
+		return self:Reduce(thisOffset, size, 0, function(r, f) return r + f end)
+	end
+
+	function self:Max(thisOffset, size)
+		return self:Reduce(thisOffset, size, 0, function(r, f) return math.max(r, f) end)
+	end
+
+	function self:CopyT(thisOffset, that, thatOffset, size)
+		return self:Reduce(thisOffset, size, 0, function(r, f)  end)
+	end
+	
+	return self
+end
+
+
+local function loadQuantized(entry)
+	local info, name = ggml_type_from_enum(entry.ggml_type)
+
+	if name == "Q4_0" then
+		return Q4_0FloatTensor(entry.size, entry.blob)
+	else
+		error("unsupported quant format " .. name)
+	end
+end
+
+loadQuantized(gguf.tensors["token_embd.weight"])
