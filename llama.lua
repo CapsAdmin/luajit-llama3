62229eedf4cb3282d42aa1a74ab15a6649c69015
CapsAdmin <eliashogstvedt@gmail.com>
2024-06-07 12:59:34 +0700

    refactoring

--- llama.lua
+++ llama.lua
@@ -5,7 +5,6 @@ local get_time = require("debug.get_time")
 local measure = require("debug.measure")
 local gguf = require("gguf")
 local Tokenizer = require("tokenizer")
-local Weights = require("weights")
 local Tensor = require("tensor")
 local Sampler = require("topp_sampler")
 
@@ -27,15 +26,14 @@ local function load_and_run(model_path, prompt, token_callback)
 		local hidden_dim = metadata["llama.feed_forward_length"]
 		local number_of_layers = metadata["llama.block_count"]
 		local number_of_heads = metadata["llama.attention.head_count"]
-		local number_of_key_value_heads = metadata["llama.attention.head_count_kv"] and
-			metadata["llama.attention.head_count_kv"] or
+		local number_of_key_value_heads = metadata["llama.attention.head_count_kv"] or
 			metadata["llama.attention.head_count"]
 		local vocabulary_size = #metadata["tokenizer.ggml.tokens"]
 		local rms_norm_eps = metadata["llama.attention.layer_norm_rms_epsilon"] or 1e-5
 		local head_size = dim / number_of_heads
+		local sqrt_head_size = math.sqrt(head_size)
 		local kv_dim = (dim * number_of_key_value_heads) / number_of_heads
 		local kv_mul = number_of_heads / number_of_key_value_heads
-		local sqrt_head_size = math.sqrt(head_size)
 		local rope_theta = metadata["llama.rope.freq_base"] or 10000
 
 		local function build_rope_freqs(context_length, rope_theta)
@@ -66,18 +64,18 @@ local function load_and_run(model_path, prompt, token_callback)
 		local function forward(state, weights)
 			weights.token_embedding_table:CopyTo(state.token * dim, state.x, 0, dim)
 
-			for l = 1, number_of_layers do
-				local key_cache = state.key_cache[l]
-				local val_cache = state.val_cache[l]
-				state.xb:RmsNormInPlace(state.x, weights.rms_att_weight[l], dim, rms_norm_eps)
-				weights.wq[l]:MatrixVectorMultiply(state.xb, state.q, dim, dim)
-				weights.wk[l]:MatrixVectorMultiply(state.xb, state.k, kv_dim, dim)
-				weights.wv[l]:MatrixVectorMultiply(state.xb, state.v, kv_dim, dim)
+			for i, layer in ipairs(weights.layers) do
+				local key_cache = state.key_cache[i]
+				local val_cache = state.val_cache[i]
+				state.xb:RmsNormInPlace(state.x, layer.rms_att_weight, dim, rms_norm_eps)
+				layer.wq:MatrixVectorMultiply(state.xb, state.q, dim, dim)
+				layer.wk:MatrixVectorMultiply(state.xb, state.k, kv_dim, dim)
+				layer.wv:MatrixVectorMultiply(state.xb, state.v, kv_dim, dim)
 
 				for i = 0, dim - 1, 2 do
-					local head_dim = i % head_size
-					local fcr = rope_freqs_real[1 + ((state.token_pos * (head_size / 2) + (head_dim / 2)))]
-					local fci = rope_freqs_imag[1 + ((state.token_pos * (head_size / 2) + (head_dim / 2)))]
+					local offset = 1 + (state.token_pos * (head_size / 2) + ((i % head_size) / 2))
+					local fcr = rope_freqs_real[offset]
+					local fci = rope_freqs_imag[offset]
 					local rotn = i < kv_dim and 2 or 1
 
 					for v = 0, rotn - 1 do
@@ -95,33 +93,37 @@ local function load_and_run(model_path, prompt, token_callback)
 				for h = 0, number_of_heads - 1 do
 					local qOffset = h * head_size
 					local attOffset = h * context_length
+					local key_cache_offset = floor(h / kv_mul) * head_size
 
 					for t = 0, state.token_pos do
-						local key_cache_offset = t * kv_dim + floor(h / kv_mul) * head_size
-						local score = state.q:Dot(qOffset, key_cache, key_cache_offset, head_size)
-						score = score / sqrt_head_size
-						state.att:SetFloat(attOffset + t, score)
+						state.att:SetFloat(
+							attOffset + t,
+							state.q:Dot(qOffset, key_cache, t * kv_dim + key_cache_offset, head_size) / sqrt_head_size
+						)
 					end
 
 					state.att:SoftMaxInPlace(attOffset, state.token_pos + 1)
-					local xbOffset = h * head_size
-					state.xb:FillInPlace(xbOffset, head_size, 0)
+					state.xb:FillInPlace(qOffset, head_size, 0)
 
 					for t = 0, state.token_pos do
-						local vOffset = t * kv_dim + floor(h / kv_mul) * head_size
-						local a = state.att:GetFloat(attOffset + t)
-						state.xb:SaxpyInPlace(xbOffset, val_cache, vOffset, head_size, a)
+						state.xb:SaxpyInPlace(
+							qOffset,
+							val_cache,
+							t * kv_dim + key_cache_offset,
+							head_size,
+							state.att:GetFloat(attOffset + t)
+						)
 					end
 				end
 
-				weights.wo[l]:MatrixVectorMultiply(state.xb, state.xb2, dim, dim)
+				layer.wo:MatrixVectorMultiply(state.xb, state.xb2, dim, dim)
 				state.x:AddTensorInPlace(state.xb2)
-				state.xb:RmsNormInPlace(state.x, weights.rms_ffn_weight[l], dim, rms_norm_eps)
-				weights.w1[l]:MatrixVectorMultiply(state.xb, state.hb, hidden_dim, dim)
-				weights.w3[l]:MatrixVectorMultiply(state.xb, state.hb2, hidden_dim, dim)
+				state.xb:RmsNormInPlace(state.x, layer.rms_ffn_weight, dim, rms_norm_eps)
+				layer.w1:MatrixVectorMultiply(state.xb, state.hb, hidden_dim, dim)
+				layer.w3:MatrixVectorMultiply(state.xb, state.hb2, hidden_dim, dim)
 				state.hb:SigmoidInPlace()
 				state.hb:MultiplyTensorInPlace(state.hb2)
-				weights.w2[l]:MatrixVectorMultiply(state.hb, state.xb, dim, hidden_dim)
+				layer.w2:MatrixVectorMultiply(state.hb, state.xb, dim, hidden_dim)
 				state.x:AddTensorInPlace(state.xb)
 			end
 
@@ -131,8 +133,40 @@ local function load_and_run(model_path, prompt, token_callback)
 
 		local tokenizer = Tokenizer:new(metadata["tokenizer.ggml.tokens"], metadata["tokenizer.ggml.merges"])
 		local sampler = Sampler:new(vocabulary_size, temperature, topp)
-		local tokens = tokenizer:EncodeString(prompt)
-		local weights = Weights(tensors, number_of_layers)
+		local prompt_tokens = tokenizer:EncodeString(prompt)
+		local weights = {layers = {}}
+
+		do
+			local function load_tensor(name)
+				local entry = tensors[name]
+
+				if Tensor[entry.type_info.name] then
+					return Tensor[entry.type_info.name](Tensor, entry.size, entry.blob):SetName(name .. "[" .. entry.type_info.name .. "]")
+				end
+
+				error("NYI tensor type: " .. entry.type_info.name)
+			end
+
+			weights.token_embedding_table = load_tensor("token_embd.weight")
+
+			for i = 0, number_of_layers - 1 do
+				weights.layers[i + 1] = {
+					rms_att_weight = load_tensor("blk." .. i .. ".attn_norm.weight"),
+					wq = load_tensor("blk." .. i .. ".attn_q.weight"),
+					wk = load_tensor("blk." .. i .. ".attn_k.weight"),
+					wv = load_tensor("blk." .. i .. ".attn_v.weight"),
+					wo = load_tensor("blk." .. i .. ".attn_output.weight"),
+					rms_ffn_weight = load_tensor("blk." .. i .. ".ffn_norm.weight"),
+					w1 = load_tensor("blk." .. i .. ".ffn_gate.weight"),
+					w2 = load_tensor("blk." .. i .. ".ffn_down.weight"),
+					w3 = load_tensor("blk." .. i .. ".ffn_up.weight"),
+				}
+			end
+
+			weights.rms_final_weight = load_tensor("output_norm.weight")
+			weights.wcls = load_tensor("output.weight")
+		end
+
 		local state = {
 			token = tokenizer:EncodeString("<|begin_of_text|>")[1] - 1,
 			token_pos = 0,
@@ -180,7 +214,6 @@ local function load_and_run(model_path, prompt, token_callback)
 
 				for _, tensor in ipairs(tensors) do
 					tensor.blob.gpu_ptr = gpu_ptr
-					gpu.copy_to_device(gpu_ptr, tensor.blob.blob, byte_size)
 				end
 
 				total_size = total_size + byte_size
@@ -211,8 +244,8 @@ local function load_and_run(model_path, prompt, token_callback)
 			forward(state, weights)
 			local next_token
 
-			if state.token_pos < #tokens then
-				next_token = tokens[state.token_pos + 1]
+			if state.token_pos < #prompt_tokens then
+				next_token = prompt_tokens[state.token_pos + 1]
 			else
 				next_token = sampler:SampleToken(state.logits) + 1
 			end
@@ -223,7 +256,9 @@ local function load_and_run(model_path, prompt, token_callback)
 				local token_string = tokenizer:TokenToString(state.token + 1)
 				token_callback(token_string)
 
-				if state.token_pos >= #tokens and token_string == "<|eot_id|>" then break end
+				if state.token_pos >= #prompt_tokens and token_string == "<|eot_id|>" then
+					break
+				end
 			end
 
 			state.token_pos = state.token_pos + 1
@@ -260,11 +295,7 @@ local function load_and_run(model_path, prompt, token_callback)
 	end
 end
 
-local prompt = [[<|start_header_id|>user<|end_header_id|>
-]] .. prompt .. [[<|eot_id|><|start_header_id|>assistant<|end_header_id|>
-]]
-
-load_and_run(model_path, prompt, function(token)
+load_and_run(model_path, "<|start_header_id|>user<|end_header_id|>\n" .. prompt .. "<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n", function(token)
 	io.write(token)
 	io.flush()
 end)
